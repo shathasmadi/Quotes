e15f1b8bc8cda27d69d53362816f5c4e7abaf35d
shathasmadi <alsmadishatha@gmail.com>
2021-08-04 16:52:35 +0300

    solve lab 8

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Quotes
+## Description 
+This app shows random popular book quotes with the author name, It chooses one quote each time it is run. And I used GSON to parse the .json file.
+
+
+## Installing Gson dependancies
+
+    1-open the application with editor.
+    2-open the build.gradle .
+    3-under dependancies add this line:
+
+    implementation 'com.google.code.gson:gson:2.8.7'
+
+    4-refresh the gradle and your Gson is ready to use.
---
+++ app/src/main/java/Quotes/Quote.java
@@ -0,0 +1,60 @@
+package Quotes;
+
+import java.util.ArrayList;
+
+public class Quote {
+    private ArrayList<String> tags;
+    private String author ;
+    private String likes;
+    private String text;
+
+    public Quote(ArrayList<String> tags, String author, String likes, String text) {
+        this.tags = tags;
+        this.author = author;
+        this.likes = likes;
+        this.text = text;
+    }
+
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "tags=" + tags +
+                ", author='" + author + '\'' +
+                ", likes='" + likes + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
